2a69404b888999f8b9ab35fa849073b10d1fb0b3
Mounir-Bmz <mounir.boumaza.dev@gmail.com>
2025-05-04 16:08:30 +0200

    Add React Router and configure routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
     return (
         <header>
             <h1>Shiny</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <span> | </span>
+                <Link to="/freelances">Freelances</Link>
+                <span> | </span>
+                <Link to="/survey/1">Survey</Link>
+            </nav>
         </header>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import Freelances from './pages/Freelances';
+import Survey from './pages/Survey';
+import Error from './components/Error';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/freelances" element={<Freelances />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
